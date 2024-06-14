32a6b3c06f5ecb2bdef7001de50537aed26b43c4
HusenGulam <hsencodung77@gmail.com>
2024-06-14 04:29:16 -0700

    brinchi github projecti

---
+++ idnex.js
@@ -0,0 +1,96 @@
+
+
+
+
+// let i = 0;
+
+// while(i <= 10){
+    
+    
+//     console.log(i);
+//     i++
+// }
+
+// let row = document.getElementById("row");
+
+// for(let c = 0; c <= 100;c++){
+//  let box = document.createElement("div")
+//  let h3 = document.createElement("h3");
+//  h3.innerHTML = "Hello World"
+// box.classList = "box"
+// row.appendChild(box)
+// box.appendChild(h3)
+// }
+
+
+// console.log(row);
+
+
+
+
+
+
+
+// let a = 10
+// if(a == 10){
+//     console.log("rost");
+// }
+// else{
+//     console.log("yolg'on");
+// }
+
+
+
+
+
+// function firstFunction(){
+//     let row = document.getElementById("row");
+
+//     for(let c = 0; c <= 10;c++){
+//     let box = document.createElement("div")
+//     let h3 = document.createElement("h3");
+//     h3.innerHTML = "Hello World"
+//     box.classList = "box"
+//     row.appendChild(box)
+//     box.appendChild(h3)
+// }
+
+// }
+
+
+// let btn = document.getElementById("btn");
+
+// btn.addEventListener("click",function(){
+//     firstFunction()
+// })
+
+
+
+
+// function hello(){
+//    let age = +prompt("youre age...");
+//    let row = document.getElementById("row");
+//    let h3 = document.createElement("h3");
+//    if(age >= 18){
+//        h3.innerHTML = "Xush kelipsiz";
+//        row.append(h3);
+//        row.style.backgroundColor = "green"
+//    }
+//    else{
+//         h3.innerHTML = "Mumkin emas";
+//         row.append(h3);
+//         row.style.backgroundColor = "red"
+//    }
+ 
+
+// }
+
+
+// hello()
+
+
+
+
+// setInterval(function(){
+//     console.log("ishladi 😊");
+// },5000)
